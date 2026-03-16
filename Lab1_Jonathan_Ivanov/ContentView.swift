4e4cf3110ab0ad86615d074f80947e484394d8e9
Your Name <your@example.com>
2026-03-15 20:44:19 -0400

    Initial Commit

---
+++ Lab1_Jonathan_Ivanov/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab1_Jonathan_Ivanov
+//
+//  Created by Jonathan Ivanov on 2026-03-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab1_Jonathan_Ivanov/Lab1_Jonathan_IvanovApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Jonathan_IvanovApp.swift
+//  Lab1_Jonathan_Ivanov
+//
+//  Created by Jonathan Ivanov on 2026-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Jonathan_IvanovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
